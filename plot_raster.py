dae99075690b28a7c1dc85daf70e25c3897202ae
Tyler Banks <tbanks@mail.missouri.edu>
2020-11-17 18:42:45 -0600

    working

---
+++ plot_raster.py
@@ -0,0 +1,11 @@
+from bmtk.analyzer.spike_trains import plot_raster
+import sys
+
+def plot_rast(config_file):
+    _ = plot_raster(config_file=config_file)
+
+if __name__ == '__main__':
+    if __file__ != sys.argv[-1]:
+        plot_rast(sys.argv[-1])
+    else:
+        plot_rast('simulation_config.json')
