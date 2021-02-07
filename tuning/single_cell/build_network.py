23be10157b1dd4a15a5958b547b28db0dac51932
Tyler Banks <tjb1991@gmail.com>
2021-02-06 18:30:48 -0600

    'single'

---
+++ tuning/single_cell/build_network.py
@@ -0,0 +1,231 @@
+from bmtk.builder import NetworkBuilder
+import numpy as np
+from bmtk.builder.auxi.node_params import positions_cuboid, positions_list, xiter_random
+import pdb
+import random
+import pandas as pd
+
+#A bunch of junk for importing the synapses loader
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+sys.path.insert(0, parentdir)
+import synapses
+import build_input
+
+np.random.seed(123412)
+
+# Initialize our network
+net = NetworkBuilder("BLA")
+
+scale = 1
+
+all_synapses = pd.DataFrame([],columns=['source_gid','target_gid'])
+
+#Number of cells in each population
+numPN_A = 1
+numPN_C = 1
+numBask = 1
+num_cells = numPN_A + numPN_C + numBask
+
+dist_constraint = False
+min_conn_dist = 0.0
+max_conn_dist = 9999.9
+
+if dist_constraint:
+    max_conn_dist = 300.0 #9999.9# Distance constraint for all cells
+
+# Load synapse dictionaries
+synapses.load()
+syn = synapses.syn_params_dicts('../../components/synaptic_models')
+###################################################################################
+####################################Pyr Type A#####################################
+
+# Add a population of numPN_A nodes (all of which share model_type, dynamics_params, etc.)
+net.add_nodes(N=numPN_A, pop_name='PyrA',
+              mem_potential='e',
+              model_type='biophysical',
+              #model_template='hoc:feng_typeA',#Ben's model
+              model_template='hoc:Cell_Af',
+              morphology=None)
+
+
+##################################################################################
+###################################Pyr Type C#####################################
+
+# Add a population of numPN_A nodes (all of which share model_type, dynamics_params, etc.)
+net.add_nodes(N=numPN_C, pop_name='PyrC',
+              mem_potential='e',
+              model_type='biophysical',
+              #model_template='hoc:feng_typeC',#Ben's model
+              model_template='hoc:Cell_Cf',
+              morphology=None)
+
+#################################################################################
+###########################Fast - spiking PV ints################################
+
+# Add a population of numBask nodes
+net.add_nodes(N=numBask, pop_name='Bask',
+              mem_potential='e',
+              model_type='biophysical',
+              #model_template='hoc:basket',#Ben's model
+              model_template='hoc:InterneuronCellf',
+              morphology=None)
+
+
+################################################################################
+############################# BACKGROUND INPUTS ################################
+
+# External inputs
+thalamus = NetworkBuilder('mthalamus')
+thalamus.add_nodes(N=numPN_A+numPN_C,
+                   pop_name='tON',
+                   pop_group='mthalamus',
+                   potential='exc',
+                   model_type='virtual')
+
+# External inputs
+exc_bg_bask = NetworkBuilder('exc_bg_bask')
+exc_bg_bask.add_nodes(N=numBask,
+                   pop_name='tON',
+                   pop_group='exc_bg_bask',
+                   potential='exc',
+                   model_type='virtual')
+
+##############################################################################
+############################## CONNECT CELLS #################################
+
+def one_to_one(source, target):
+    
+    sid = source.node_id
+    tid = target.node_id
+    if sid == tid:
+    #print("connecting cell {} to {}".format(sid,tid))
+        tmp_nsyn = 1
+    else:
+        return None
+
+    return tmp_nsyn
+
+def one_to_one_offset(source, target, offset=0):
+
+    sid = source.node_id
+    tid = target.node_id - offset
+    if sid == tid:
+        #print("connecting cell {} to {}".format(sid,tid))
+        tmp_nsyn = 1
+    else:
+        #print("NOT connecting cell {} to {}".format(sid,tid))
+        return None
+
+    return tmp_nsyn
+
+def syn_uniform_delay_section(source, target, sec_id=0, sec_x=0.9, mean=0.5,std=1):
+    return np.random.uniform(mean,std), sec_id, sec_x
+
+
+##########################################################################
+######################### BACKGROUND INPUT ###############################
+
+
+dynamics_file='BG2PNe_feng_min.json'
+#dynamics_file = 'BG2PN_feng_min.json'
+
+conn = net.add_edges(source=thalamus.nodes(), target=net.nodes(pop_name='PyrA'),
+                   connection_rule=one_to_one,
+                   syn_weight=1,
+                   target_sections=['basal'],
+                   #delay=0.1,
+                   distance_range=[0.0, 9999.9],
+                   dynamics_params=dynamics_file,
+                   model_template=syn[dynamics_file]['level_of_detail'])
+                   #dynamics_params='BG2PNe_feng.json',
+                   #model_template='bg2pyr',
+                   #sec_x=0.9)
+
+conn.add_properties(names=['delay','sec_id','sec_x'],
+                  rule=syn_uniform_delay_section,
+                  rule_params={'sec_id':1, 'sec_x':0.9},
+                  dtypes=[np.float, np.int32, np.float])
+
+conn = net.add_edges(source=thalamus.nodes(), target=net.nodes(pop_name='PyrC'),
+                   connection_rule=one_to_one,
+                   syn_weight=1,
+                   target_sections=['basal'],
+                   #delay=0.1,
+                   distance_range=[0.0, 9999.9],
+                   dynamics_params=dynamics_file,
+                   model_template=syn[dynamics_file]['level_of_detail'])
+                   #dynamics_params='BG2PNe_feng.json',
+                   #model_template='bg2pyr',
+                   #sec_x=0.9)
+
+conn.add_properties(names=['delay','sec_id','sec_x'],
+                  rule=syn_uniform_delay_section,
+                  rule_params={'sec_id':1, 'sec_x':0.9},
+                  dtypes=[np.float, np.int32, np.float])
+
+
+dynamics_file = 'BG2PNi_feng_min.json'
+#dynamics_file = 'BG2INT_feng_min.json'
+
+conn = net.add_edges(source=exc_bg_bask.nodes(), target=net.nodes(pop_name='Bask'),
+                   connection_rule=one_to_one_offset,
+                   connection_params={'offset':numPN_A+numPN_C},
+                   syn_weight=1,
+                   target_sections=['basal'],
+                   #delay=0.1,
+                   distance_range=[0.0, 9999.9],
+                   dynamics_params=dynamics_file,
+                   model_template=syn[dynamics_file]['level_of_detail'])
+                   #dynamics_params='BG2PNi_feng.json',
+                   #model_template='bg2pyr',
+                   #sec_x=0.9)
+
+
+conn.add_properties(names=['delay','sec_id','sec_x'],
+                  rule=syn_uniform_delay_section,
+                  rule_params={'sec_id':1, 'sec_x':0.9},
+                  dtypes=[np.float, np.int32, np.float])
+
+
+
+##########################################################################
+###############################  BUILD  ##################################
+
+
+net.build()
+net.save_nodes(output_dir='network')
+net.save_edges(output_dir='network')
+
+print("Internal nodes and edges built")
+
+# Create connections between "thalamus" and Pyramidals
+# First define the connection rule
+
+# Build and save our network
+
+thalamus.build()
+thalamus.save_nodes(output_dir='network')
+
+exc_bg_bask.build()
+exc_bg_bask.save_nodes(output_dir='network')
+#
+#print("External nodes and edges built")
+t_sim = 1000.0
+
+from bmtk.utils.sim_setup import build_env_bionet
+
+build_env_bionet(base_dir='./',
+		network_dir='./network',
+		tstop=t_sim, dt = 0.05,
+		report_vars = ['v'],
+                v_init = -70.0,
+                celsius = 31.0,
+		spikes_inputs=[('mthalamus',   # Name of population which spikes will be generated for
+                                'mthalamus_spikes.h5'),('exc_bg_bask','exc_bg_bask_spikes.h5')],
+		components_dir='../../components',
+		compile_mechanisms=False)
+
+
+build_input.build_input(t_sim,numPN_A,numPN_C,2)
---
+++ tuning/single_cell/run_network.py
@@ -0,0 +1,62 @@
+import os, sys
+from bmtk.simulator import bionet
+import numpy as np
+
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+sys.path.insert(0, parentdir)
+import synapses
+import warnings
+
+
+def run(config_file):
+
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    synapses.load()
+    from bmtk.simulator.bionet.pyfunction_cache import add_weight_function
+
+    def gaussianBL(edge_props, source, target):
+        w0 = edge_props["syn_weight"]
+        sigma = edge_props["weight_sigma"]
+        return np.random.normal(w0, sigma, 1)
+	
+    def lognormal(edge_props, source, target):
+        m = edge_props["syn_weight"]
+        s = edge_props["weight_sigma"]
+        mean = np.log(m) - 0.5 * np.log((s/m)**2+1)
+        std = np.sqrt(np.log((s/m)**2 + 1))
+        return np.random.lognormal(mean, std, 1)
+
+    add_weight_function(lognormal)
+    add_weight_function(gaussianBL)
+
+
+    conf = bionet.Config.from_json(config_file, validate=True)
+    conf.build_env()
+
+    graph = bionet.BioNetwork.from_config(conf)
+
+    # This fixes the morphology error in LFP calculation
+    pop = graph._node_populations['BLA']
+    for node in pop.get_nodes():
+        node._node._node_type_props['morphology'] = node.model_template[1]
+
+    sim = bionet.BioSimulator.from_config(conf, network=graph)
+    
+    # This calls insert_mechs() on each cell to use its gid as a seed
+    # to the random number generator, so that each cell gets a different
+    # random seed for the point-conductance noise
+    cells = graph.get_local_cells()
+    for cell in cells:
+        cells[cell].hobj.insert_mechs(cells[cell].gid)
+    
+    sim.run()
+    bionet.nrn.quit_execution()
+
+
+if __name__ == '__main__':
+    if __file__ != sys.argv[-1]:
+        run(sys.argv[-1])
+    else:
+        run('simulation_config.json')
