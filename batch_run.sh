1551a101418d09a7b164c75daa5dd0e5fa8518ee
Tyler Banks <tbanks@mail.missouri.edu>
2021-08-25 16:35:03 -0500

    fix attach

--- batch_run.sh
+++ batch_run.sh
@@ -11,6 +11,6 @@ START=$(date)
 mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_base_edge_effects.json
 END=$(date)
 
-{ printf "Start: $START \nEnd:   $END\n" & python analysis.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta Simulation Results" -A analysis.png tbg28@mail.missouri.edu
+{ printf "Start: $START \nEnd:   $END\n" & python analysis.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta Simulation Results" -a analysis.png tbg28@mail.missouri.edu
 
 echo "Done running model at $(date)"
