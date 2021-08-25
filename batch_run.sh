b5536f6e3701c4763904e8bbcc9d43573fbfe72b
Tyler Banks <34382569+tjbanks@users.noreply.github.com>
2021-08-25 14:50:25 -0500

    Update batch_run.sh

--- batch_run.sh
+++ batch_run.sh
@@ -4,13 +4,13 @@
 #SBATCH -n 50
 #SBATCH --qos=normal
 #SBATCH --job-name=amygdala_theta
-#SBATCH --output=ca1%j.out
-#SBATCH --time 0-00:30
+#SBATCH --output=amygdala_batch_%j.out
+#SBATCH --time 0-12:00
 
 START=$(date)
 mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_base_edge_effects.json
 END=$(date)
 
-{ printf "Start: $START \nEnd:   $END\n" & python analysis.py -no-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta Simulation Results" tbg28@mail.missouri.edu
+{ printf "Start: $START \nEnd:   $END\n" & python analysis.py -no-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta Simulation Results" -A analysis.png tbg28@mail.missouri.edu
 
 echo "Done running model at $(date)"
