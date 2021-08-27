5a35d8f5233f4f18fe2932c6f4d9f25706a4b250
Tyler Banks <tbanks@mail.missouri.edu>
2021-08-27 16:56:34 -0500

    vpsi scale up and batch script

---
+++ batch_vpsi_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -n 50
+#SBATCH --qos=normal
+#SBATCH --job-name=amygdala_theta
+#SBATCH --output=amygdala_batch_%j.out
+#SBATCH --time 0-24:00
+
+START=$(date)
+mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_base_edge_effects.json
+END=$(date)
+
+{ printf "Start: $START \nEnd:   $END\n" & python analysis.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta VPSI Simulation Results" -a analysis.png tbg28@mail.missouri.edu
+
+START=$(date)
+mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_vpsi_edge_effects.json
+END=$(date)
+
+{ printf "Start: $START \nEnd:   $END\n" & python analysis.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta Baseline Simulation Results" -a analysis.png tbg28@mail.missouri.edu
+
+
+
