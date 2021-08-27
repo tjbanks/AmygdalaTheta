8a1bec99947da92453c4dea197f034a6cfb558fa
Tyler Banks <tbanks@mail.missouri.edu>
2021-08-27 17:01:04 -0500

    swapped runs

--- batch_vpsi_run.sh
+++ batch_vpsi_run.sh
@@ -8,13 +8,13 @@
 #SBATCH --time 0-24:00
 
 START=$(date)
-mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_base_edge_effects.json
+mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_vpsi_edge_effects.json
 END=$(date)
 
 { printf "Start: $START \nEnd:   $END\n" & python analysis.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta VPSI Simulation Results" -a analysis.png tbg28@mail.missouri.edu
 
 START=$(date)
-mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_vpsi_edge_effects.json
+mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_base_edge_effects.json
 END=$(date)
 
 { printf "Start: $START \nEnd:   $END\n" & python analysis.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta Baseline Simulation Results" -a analysis.png tbg28@mail.missouri.edu
