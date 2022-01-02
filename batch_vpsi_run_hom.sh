b2a719081d09cfeffb8be5561f6375906b018593
Tyler Banks <tbanks@mail.missouri.edu>
2022-01-01 19:43:35 -0600

    diff hom

--- batch_vpsi_run_hom.sh
+++ batch_vpsi_run_hom.sh
@@ -11,7 +11,7 @@ START=$(date)
 mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_vpsi_homogenous.json
 END=$(date)
 
-{ printf "Start: $START \nEnd:   $END\n" & python analysis_hom.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta VPSI Simulation Results" -a analysis.png tbg28@mail.missouri.edu
+{ printf "Start: $START \nEnd:   $END\n" & python analysis_hom.py --save-plots & printf "\n\n" & git diff components_homogenous/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta VPSI Simulation Results" -a analysis.png tbg28@mail.missouri.edu
 
 
 
