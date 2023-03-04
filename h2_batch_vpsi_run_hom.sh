bb1ff3be77a35030663420318272d9cd0ca1d57f
Tyler Banks <tbanks@mail.missouri.edu>
2023-03-04 10:45:56 -0600

    cleanup

--- h2_batch_vpsi_run_hom.sh
+++ h2_batch_vpsi_run_hom.sh
@@ -8,7 +8,8 @@
 #SBATCH --time 0-24:00
 
 START=$(date)
-mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_vpsi_homogenous.json
+#mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_vpsi_homogenous.json
+mpiexec ./components/mechanisms/x86_64/special -mpi run_network.py simulation_configECP_vpsi_homogenous.json
 END=$(date)
 
 { printf "Start: $START \nEnd:   $END\n" & python analysis_hom.py --save-plots & printf "\n\n" & git diff components_homogenous/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta VPSI Simulation Results" -a analysis.png tbg28@mail.missouri.edu
