eb2b39569d0a29f02664e3e50c0b062e4e5fb26b
Tyler Banks <tbanks@login02.expanse.sdsc.edu>
2023-04-05 21:52:44 -0700

    expanse h1

--- exp_h1_batch_run_hom.sh
+++ exp_h1_batch_run_hom.sh
@@ -14,6 +14,8 @@ START=$(date)
 mpiexec ./components_homogenous/mechanisms/x86_64/special -mpi run_network.py simulation_configECP_base_homogenous.json
 END=$(date)
 
-{ printf "Start: $START \nEnd:   $END\n" & python analysis_hom.py --save-plots & sleep 20 & printf "\n\n" & git diff components_homogenous/synaptic_models/; }| mail -r tbg28@mail.missouri.edu -s "AmygdalaTheta Simulation Results" -a analysis.png tbg28@mail.missouri.edu
+printf "Start: $START \nEnd:   $END\n"
+python analysis_hom.py --save-plots
 
 echo "Done running model at $(date)"
+
