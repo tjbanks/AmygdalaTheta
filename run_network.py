49f24e4e8f74a01336f64f2633cfd006234e40f2
Tyler Banks <tjb1991@gmail.com>
2023-06-15 15:44:47 -0500

    ACH code

--- run_network.py
+++ run_network.py
@@ -11,7 +11,7 @@ try:
 except:
     pass
 
-def run(config_file, coreneuron=True, gpu=False):
+def run(config_file, coreneuron=False, gpu=False):
 
     warnings.simplefilter(action='ignore', category=FutureWarning)
     synapses.load()
@@ -60,6 +60,45 @@ def run(config_file, coreneuron=True, gpu=False):
         cells[cell].hobj.insert_mechs(cells[cell].gid)
         pass
     
+    # Setup ACh
+    # add a line to config 
+    #   "ACH_level": 0, 1, 2
+    # to start executing this
+    if conf.get('ACH_level'):
+        # 1. Get the list of cells that have been pre-selected as ACH receptive
+        # from the node_sets.json file
+        ach_receptive_cells = conf['node_sets'].get('ach_cells')
+        print(f"{len(ach_receptive_cells)} cells are set to be receptive to ACH")
+
+        # 2. Iterate through each cell in the network and if it's receptive, alter the synapse
+        total_modified_synapses = 0
+        num_modified_synapses = 0
+        ach_receptive_property = "bACH"
+        ach_recpetive_property_on_value = conf.get("ACH_level", 0) #default no ACH delivered
+        for cell_id, cell in graph._rank_node_ids.items():
+            if cell_id in ach_receptive_cells:
+                cell_connections = cell.connections()
+                for connection in cell_connections:
+                    syn = connection._syn
+                    if hasattr(syn, ach_receptive_property):
+                        setattr(syn, ach_receptive_property, ach_recpetive_property_on_value)
+                        num_modified_synapses += 1
+                total_modified_synapses += len(cell_connections)
+
+        print(f"ACH turned on for {len(ach_receptive_cells)} muscarinic (synaptic) receptors")
+        print(f"{num_modified_synapses}/{total_modified_synapses} ACH synapses modified")
+        print(f"{ach_receptive_property} set to value {ach_recpetive_property_on_value}")
+        
+        # 3. Turn on nicotinic receptors per cell
+        nic_cells_turned_on = 0
+        for cell in ach_receptive_cells:
+            hobj = cells[cell].hobj
+            if hasattr(hobj, 'activate_ach'):
+                hobj.activate_ach()
+                nic_cells_turned_on += 1
+        print(f"{nic_cells_turned_on}/{len(ach_receptive_cells)} nicotinic (intrinsic) receptors turned on")
+
+
     sim.run()
     bionet.nrn.quit_execution()
 
